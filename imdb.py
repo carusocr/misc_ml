0476eaaf8b19bf3b34197fa7967cb135620f608a
Chris Caruso <carusochrisr@gmail.com>
2019-02-02 18:42:55 -0800

    imdb

--- imdb.py
+++ imdb.py
@@ -1,5 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.datasets import imdb
+from keras import models
+from keras import layers
 
 def vectorize_sequences(sequences, dimension=10000):
   results = np.zeros((len(sequences), dimension))
@@ -10,14 +13,45 @@ def vectorize_sequences(sequences, dimension=10000):
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
 
-word_index = imdb.get_word_index()
+#word_index = imdb.get_word_index()
 
-reverse_word_index = dict([(value, key) for (key,value) in word_index.items()])
+#reverse_word_index = dict([(value, key) for (key,value) in word_index.items()])
 
-decoded_review = ''.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+#decoded_review = ''.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['loss_values']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label="Training Loss")
+plt.plot(epochs, val_loss_values, 'b', label="Validation Loss")
+plt.title("Training and Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+
+plt.show()
