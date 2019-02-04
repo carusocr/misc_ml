a1724b53ea353263106e33bd46363d76b499ebd2
Chris Caruso <carusochrisr@gmail.com>
2019-02-03 17:11:33 -0800

    imdb tweakage

--- imdb.py
+++ imdb.py
@@ -4,14 +4,14 @@ from keras.datasets import imdb
 from keras import models
 from keras import layers
 
-def vectorize_sequences(sequences, dimension=10000):
+def vectorize_sequences(sequences, dimension=1000):
   results = np.zeros((len(sequences), dimension))
   for i, sequence in enumerate(sequences):
     results[i, sequence] = 1.
   return results
 
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 1000)
 
 #word_index = imdb.get_word_index()
 
@@ -26,30 +26,45 @@ y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(64, activation='relu', input_shape=(1000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = y_train[:1000]
+partial_y_train = y_train[1000:]
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+history = model.fit(partial_x_train, partial_y_train, epochs=16, batch_size=512, validation_data=(x_val, y_val))
 
 history_dict = history.history
 loss_values = history_dict['loss']
-val_loss_values = history_dict['loss_values']
+val_loss_values = history_dict['val_loss']
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
 
 epochs = range(1, len(acc) + 1)
 
 plt.plot(epochs, loss_values, 'bo', label="Training Loss")
 plt.plot(epochs, val_loss_values, 'b', label="Validation Loss")
 plt.title("Training and Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+
+plt.show()
+
+plt.clf()
+
+plt.plot(epochs, acc, 'bo', label="Training Acc")
+plt.plot(epochs, val_acc, 'b', label="Validation Acc")
+plt.title("Training vs Validation Accuracy")
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend()
