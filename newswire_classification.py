875e235d8f428b9172981bfc4af93095dc3f536b
Chris Caruso <carusochrisr@gmail.com>
2019-02-14 17:05:51 -0800

    newswire

--- newswire_classification.py
+++ newswire_classification.py
@@ -37,8 +37,8 @@ one_hot_test_labels = to_categorical(test_labels)
 
 
 model = models.Sequential()
-model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(128, activation='relu', input_shape=(10000,)))
+#model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(46, activation='softmax'))
 
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -50,10 +50,12 @@ partial_x_train = x_train[1000:]
 y_val = one_hot_train_labels[:1000]
 partial_y_train = one_hot_train_labels[1000:]
 
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+history = model.fit(partial_x_train, partial_y_train, epochs=9, batch_size=512, validation_data=(x_val, y_val))
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
 
 epochs = range(1, len(loss) + 1)
 
@@ -65,5 +67,14 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-results = model.evaluate(x_test, one_hot_test_labels)
-print results
+#results = model.evaluate(x_test, one_hot_test_labels)
+#print results
+
+plt.clf()
+plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
