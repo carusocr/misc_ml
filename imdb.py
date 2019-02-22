b70daa51a9dc92da66e5587b3083543155bbd41b
Chris Caruso <carusochrisr@gmail.com>
2019-02-21 18:27:30 -0800

    cleanup

--- imdb.py
+++ imdb.py
@@ -4,14 +4,14 @@ from keras.datasets import imdb
 from keras import models
 from keras import layers
 
-def vectorize_sequences(sequences, dimension=1000):
+def vectorize_sequences(sequences, dimension=10000):
   results = np.zeros((len(sequences), dimension))
   for i, sequence in enumerate(sequences):
     results[i, sequence] = 1.
   return results
 
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 1000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
 
 #word_index = imdb.get_word_index()
 
@@ -26,7 +26,7 @@ y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
 model = models.Sequential()
-model.add(layers.Dense(64, activation='relu', input_shape=(1000,)))
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
@@ -34,10 +34,10 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-x_val = x_train[:1000]
-partial_x_train = x_train[1000:]
-y_val = y_train[:1000]
-partial_y_train = y_train[1000:]
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
